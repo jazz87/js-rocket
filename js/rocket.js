3b02f10adde6dd1b3e7316d7c10b7324ce5c6e1a
Jasbee <info@jasbeesingh.nl>
2019-02-24 23:47:00 +0100

    Added state functions

--- js/rocket.js
+++ js/rocket.js
@@ -6,18 +6,33 @@ var countdownNumber = 10;
 var changeState = function (state) {
 	document.body.className = 'body-state'+ state;
 	clearInterval(timer);
+	countdownNumber = 10;
+	document.getElementById('countdown').innerHTML = countdownNumber;
+
 	// Countdown function
 	if (state == 2) {
 		timer = setInterval(function () {
-			document.getElementById('countdown').innerHTML = countdownNumber;
 			countdownNumber = countdownNumber-1;
-			// When zero hits, go to state 3
+			document.getElementById('countdown').innerHTML = countdownNumber;
 			if (countdownNumber <= 0){
-				
 				changeState(3);
 			};
-		}, 1000);
+		}, 500);
+	// When zero hits, go to state 3
 	} else if (state == 3) {
+		var succes = setTimeout(function () 
+		{
+				var randomNumber = Math.round(Math.random()*10);
 
+				console.log('randomNumber:',randomNumber)
+
+				// succes
+				if (randomNumber > 5) {
+					changeState(4);
+				// oh no message	
+				} else {
+					changeState(5);
+				}	
+		}, 2000);
 	};
 }
